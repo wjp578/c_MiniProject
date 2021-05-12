2a4b7ea2505346595e0eba4b7969b71d95ed2ebc
wjp578 <2923525168@qq.com>
2021-05-13 07:44:31 +0800

    5.13

---
+++ 0.Make a random plan for English learning/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TXT_NAME "English Plan.txt"
+
+int main()
+{
+    FILE* file=fopen(TXT_NAME,"w");
+    printf("请输入计划完成天数：");
+    int day;
+    scanf("%d",&day);
+    srand((unsigned int)time(NULL));
+
+    int *days=(int*)malloc(4*day);
+    int i=0;
+    for(;;)
+    {
+        int d=rand()%day+1;
+        int j=0;
+        for(j=-1;j<i;j++)
+        {
+            if(days[j+1]!=d && j+1==i)
+            {
+                days[i]=d;
+                i++;
+                break;
+            }
+        }
+        if(i==day)
+            break;
+    }
+
+    for(i=0;i<day;i++)
+    {
+        fprintf(file,"day%d\tpage%d\n",i+1,days[i]);
+    }
+
+    free(days);
+    days=NULL;
+
+    fclose(file);
+    file=NULL;
+    return 0;
+}
